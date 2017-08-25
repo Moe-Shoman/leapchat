84a56fa792f62d09f00f285ceea6f9ab674df904
Steve Phillips <steve@tryingtobeawesome.com>
2017-08-25 02:52:04 -0700

    emojis: Fixed non-Mac rendering; all emojis now load from our origin
    ...not 'unpkg.com'
    
    Emojis now show up on non-Mac devices, and do so while safely escaping the message
    
    Formatting/whitespace tweaks

--- src/components/chat/Message.js
+++ src/components/chat/Message.js
@@ -7,14 +7,21 @@ class Message extends Component {
     let { message, username } = this.props;
     let fromMe = message.from === username;
     let messageClass = fromMe ? 'chat-outgoing' : 'chat-incoming';
+
     let spaced = message.msg.replace(/:(\w+):/g, ':$1: ');
     let emojified = emoji.replace_colons(spaced);
-    let linked = md.renderInline(emojified);
+
+    // Convert `emoji.replace_colons`-generated <span> tags to Markdown
+    let emojiMD = emojified.replace(/<span class="emoji emoji-sizer" style="background-image:url\((\/static\/img\/emoji\/apple\/64\/.*?)\)" data-codepoints="(?:.*?)"><\/span>/g, '![emoji]($1)');
+
+    // Render escaped HTML/Markdown
+    let linked = md.renderInline(emojiMD);
+
     return (
       <li className={'chat-message ' + messageClass} key={message.key}>
         <span className="username">{message.from}</span>
-          <div dangerouslySetInnerHTML={{__html: linked}}>
-          </div>
+        <div dangerouslySetInnerHTML={{__html: linked}}>
+        </div>
       </li>
     );
   }
--- src/components/chat/MessageForm.js
+++ src/components/chat/MessageForm.js
@@ -4,6 +4,7 @@ import { Button } from 'react-bootstrap';
 import FaArrowCircleRight from 'react-icons/lib/fa/arrow-circle-right';
 import FaSmileO from 'react-icons/lib/fa/smile-o';
 import { Picker } from 'emoji-mart';
+import emoji from '../../constants/emoji';
 
 class MessageForm extends Component {
   constructor(props) {
@@ -73,45 +74,64 @@ class MessageForm extends Component {
     this.props.onSendMessage(message);
     this.clearMessage();
   }
+
   togglePicker = () => {
     this.setState((prevState) => {
       return {showEmojiPicker: !prevState.showEmojiPicker}
     })
   }
+
+  onClickPicker = (emoji, event) => {
+    this.addEmoji(emoji);
+    this.closePicker();
+  }
+
+  backgroundImageFn = (set, sheetSize) => {
+    if (set !== 'apple' || sheetSize !== 64) {
+      console.log('WARNING: using set "apple" and sheetSize 64 rather than',
+                  set, 'and', sheetSize, 'as was requested');
+    }
+    return '/' + emoji.EMOJI_APPLE_64_SHEET;
+  }
+
   closePicker = () => {
     this.setState({
       showEmojiPicker: false
     })
   }
+
   addEmoji = (emoji) => {
     this.setState((prevState) => {
       return {message: prevState.message.concat(`${emoji.colons} `)}
     })
   }
+
   render() {
     let { message, showEmojiPicker } = this.state;
 
     return (
       <div className="message-form">
         <form role="form" className="form" onSubmit={this.onSendMessage}>
-          {showEmojiPicker && <Picker 
-             emojiSize={24}
-             perLine={9}
-             skin={1}
-             set={'apple'}
-             autoFocus={false}
-             include={[]}
-             exclude={['nature', 'places', 'flags']}
-             emoji={""}
+          {showEmojiPicker && <Picker
+            emojiSize={24}
+            perLine={9}
+            skin={1}
+            set={'apple'}
+            autoFocus={false}
+            include={[]}
+            exclude={['nature', 'places', 'flags']}
+            emoji={""}
             title={"LeapChat"}
-            onClick={(emoji, event) => {this.addEmoji(emoji); this.closePicker()}}/>}
-          <div>
+            backgroundImageFn={this.backgroundImageFn}
+            onClick={this.onClickPicker} />}
 
+          <div>
             <div className="chat-icons">
               <FaSmileO size={24}
                 className="emoji-picker-icon"
-                onClick={this.togglePicker}/>
+                onClick={this.togglePicker} />
             </div>
+
             <div className="message">
               <textarea
                 className="form-control"
@@ -120,13 +140,14 @@ class MessageForm extends Component {
                 name="message"
                 value={message}
                 ref={(input) => { this.messageInput = input; }}
-                placeholder="Enter message" required></textarea>
-                <Button onClick={this.onSendMessage}>
-                  <FaArrowCircleRight size={30} />
-                </Button>
+                placeholder="Enter message" required>
+              </textarea>
+              <Button onClick={this.onSendMessage}>
+                <FaArrowCircleRight size={30} />
+              </Button>
             </div>
-
           </div>
+
         </form>
       </div>
     );
